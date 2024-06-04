b3914246c5d25052b0fc44b3a66ce6812f6dd20a
anandacdr <jazzw815@gmail.com>
2024-06-04 13:41:33 +0545

    App and Main

--- 01-vite_react/src/App.jsx
+++ 01-vite_react/src/App.jsx
@@ -1,15 +1,14 @@
 import Code from "./Code"
 
 function App() {
-
+  const username = "Ananda Chaudhary"
   return (
     <>
-      <h1>Vite-SANDBOX CODER | Ananda Chaudhary</h1>
+      <h1>Vite-SANDBOX CODER | {username}</h1>
       <Code />
       <p>Multiple Elements</p>
     </>
   )
-}
+}  
 
 export default App
-zz
--- 01-vite_react/src/main.jsx
+++ 01-vite_react/src/main.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+function myApp() {
+  return (
+    <div>
+      <h2>Custom App</h2>
+    </div>
+  );
+}
+
+// const reactElement = {
+//   type: 'a',
+//   props: {
+//       href: "https://github.com",
+//       target: "_blank"
+//   },
+//   children: "Click me to visit Github"
+// }
+
+const anotherElement = (
+  <a href="https://www.github.com" target="_blank">
+    Github
+  </a>
+);
+
+const anotherUser = "Pikachu";
+
+const reactElement = React.getElementById(
+  "a",
+  { href: "https://github.com", target: "_blank" },
+  "click me to visit GitHub",
+  anotherUser
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    {/* <App /> */}
+    reactElement
+  </React.StrictMode>
+);
