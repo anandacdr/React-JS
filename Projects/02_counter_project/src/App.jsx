bf1a9e202b1b1c4d362daeeb9d1999be542956c4
anandacdr <jazzw815@gmail.com>
2024-06-05 08:57:49 +0545

    Modify

--- Projects/02_counter_project/src/App.jsx
+++ Projects/02_counter_project/src/App.jsx
@@ -1,39 +1,60 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
 
-  let [counter, setCounter] = useState(0)
+  let [counter, setCounter] = useState(0);
+  let [warning, setWarning] = useState('');
 
   // let counter = 14
 
   // Add Value - Function
   const addValue = () => {
-    console.log("Clicked to Add");
-    counter = counter + 1
+    if (counter < 20){
+      console.log("Clicked to Add");
+      counter = counter + 1;
+      setWarning('');
+      setCounter(counter)
+    }
+    if (counter === 20){
+      setWarning(`Warning: Counter is at Maximum Value of 20`)
+    }
 
-    setCounter(counter)
 
-    console.log(counter)
+    // console.log("Clicked to Add");
+    // counter = counter + 1
+
+    // setCounter(counter)
+
+    // console.log(counter)
   }
 
   // Remove Value - Function
   const removeValue = () => {
-    console.log("Click to Remove")
-    setCounter(counter - 1)
 
-  }
+    if (counter > 0){
+      console.log("Click to Remove")
+      setCounter(counter - 1)
+      setWarning('');
+    }
+    if (counter === 1){
+      setWarning(`Warning: Counter is at Minimum Value of 0`)
+    }
+
+  };
   return (
     <>
       <h1>Counter Project</h1>
 
       <h2>Counter Value: {counter}</h2>
 
+      {warning && <p className='warning'>{warning}</p>}
+
       <button onClick={addValue}>Add Value</button>
       
       <button onClick={removeValue}>Remove Value</button>
+
+
         
     </>
   )
