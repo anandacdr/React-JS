ddf10c06a296a07b417c6b107ec42a49b1a12d97
anandacdr <jazzw815@gmail.com>
2024-06-09 08:19:14 +0545

    ContextAPI

--- 08_context_api/src/App.jsx
+++ 08_context_api/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <UserContextProvider>
 
-      <h2 className='bg-gray-600 text-center'>CONTEXT API | React</h2>
+      <h2 className='bg-gray-600 text-center text-3xl'>CONTEXT API | React</h2>
 
       <Login />
       <Profile />
--- 08_context_api/src/context/UserContextProvider.jsx
+++ 08_context_api/src/context/UserContextProvider.jsx
@@ -14,5 +14,4 @@ const userContextProvider = ({children}) => {
 }
 
 
-
 export default userContextProvider
