912890630a3db8c17e88dbd3eb3ae8f522d12fbe
anandacdr <jazzw815@gmail.com>
2024-06-12 16:48:10 +0545

    10- context and, methods

--- 10_to_do_web_app/src/contexts/TodoContext.js
+++ 10_to_do_web_app/src/contexts/TodoContext.js
@@ -1,9 +1,26 @@
 import { createContext, useContext } from "react";
 
-export default TodoContext = createContext({})
+export default TodoContext = createContext({
+    // Values of Context
+    todos : [
+        {
+            id: 1,
+            todo: "Todo Message",
+            completed: false
+        }
+    ],
+    // Methods of Context
+    addTodo: (todo) => {},
+    updatedTodo: (id, todo) => {},
+    deleteTodo: (id) => {},
+    toggleComplete: (id) => {}
+})
 
 
 
 export const useTodo = () => {
     return useContext(TodoContext)
 }
+
+
+export const Todoprovider = TodoContext.Provider 
